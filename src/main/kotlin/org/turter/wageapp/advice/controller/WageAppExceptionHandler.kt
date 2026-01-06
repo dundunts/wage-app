d0890db5ed7b75bf8a27d1983f60cffe0a02aac9
belbek <belbek123@gmail.com>
2026-01-06 22:09:23 +0300

    Add additional exception handlers

--- src/main/kotlin/org/turter/wageapp/advice/controller/WageAppExceptionHandler.kt
+++ src/main/kotlin/org/turter/wageapp/advice/controller/WageAppExceptionHandler.kt
@@ -4,11 +4,20 @@ import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.turter.wageapp.domain.shared.ConflictDataException
 import org.turter.wageapp.domain.shared.EntityNotFoundException
+import org.turter.wageapp.domain.shared.NotConsistenceDataException
 import org.turter.wageapp.domain.shared.NotUniqueValueException
 
 @ControllerAdvice
 class WageAppExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    suspend fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ProblemDetail> {
+        val pd = ProblemDetail.forStatus(400)
+        pd.detail = e.message
+        return ResponseEntity.status(pd.status).body(pd)
+    }
 
     @ExceptionHandler(EntityNotFoundException::class)
     suspend fun handleEntityNotFoundException(e: EntityNotFoundException): ResponseEntity<ProblemDetail> {
@@ -19,6 +28,20 @@ class WageAppExceptionHandler {
 
     @ExceptionHandler(NotUniqueValueException::class)
     suspend fun handleNotUniqueValueException(e: NotUniqueValueException): ResponseEntity<ProblemDetail> {
+        val pd = ProblemDetail.forStatus(409)
+        pd.detail = e.message
+        return ResponseEntity.status(pd.status).body(pd)
+    }
+
+    @ExceptionHandler(ConflictDataException::class)
+    suspend fun handleConflictDataException(e: ConflictDataException): ResponseEntity<ProblemDetail> {
+        val pd = ProblemDetail.forStatus(409)
+        pd.detail = e.message
+        return ResponseEntity.status(pd.status).body(pd)
+    }
+
+    @ExceptionHandler(NotConsistenceDataException::class)
+    suspend fun handleNotConsistenceDataException(e: NotConsistenceDataException): ResponseEntity<ProblemDetail> {
         val pd = ProblemDetail.forStatus(409)
         pd.detail = e.message
         return ResponseEntity.status(pd.status).body(pd)
