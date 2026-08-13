d2190c5df7ce4b28e38bc33597866d6e56370194
Daniil Konovalov <45730241+dundunts@users.noreply.github.com>
2026-08-13 17:52:41 +0300

    Remove obsolete checkpoint test adapter

--- src/test/kotlin/org/turter/wageapp/utils/checkpoint/CheckpointDtoSupplier.kt
+++ src/test/kotlin/org/turter/wageapp/utils/checkpoint/CheckpointDtoSupplier.kt
@@ -3,30 +3,11 @@ package org.turter.wageapp.utils.checkpoint
 import org.turter.wageapp.transport.model.CheckpointMetricDestination
 import org.turter.wageapp.transport.model.CheckpointMetricRecordRequest
 import org.turter.wageapp.transport.model.CheckpointType
-import org.turter.wageapp.transport.model.CreateCheckpointRequest
 import org.turter.wageapp.transport.model.UpdateCheckpointRequest
 import java.time.LocalDateTime
 import java.util.*
 
 object CheckpointDtoSupplier {
-
-    fun createRegular(
-        sessionId: UUID,
-        revenue: Int = 1000,
-        tips: Int = 100,
-        employeeIds: Set<UUID> = emptySet(),
-        dateTime: LocalDateTime = LocalDateTime.now(),
-        type: CheckpointType = CheckpointType.REGULAR,
-        fieldRecords: List<CheckpointMetricRecordRequest> = emptyList()
-    ) = CreateCheckpointRequest(
-        sessionId = sessionId,
-        revenue = revenue,
-        tips = tips,
-        employeeIds = employeeIds,
-        dateTime = dateTime.toString(),
-        type = type,
-        fieldRecords = fieldRecords
-    )
 
     fun update(
         id: UUID,
